b1eb2a6353729944b53800cec2dbd3a1924c292e
Simon Nikolaidis <s-nikolaidis@web.de>
2021-11-15 16:03:36 +0100

    ChatApp v.1.1.1

--- App/main.js
+++ App/main.js
@@ -1,65 +1,69 @@
-
-const socket = io("http://192.168.178.104:3000/", { transports : ['websocket'] });
+const socket = io("http://192.168.178.104:3000/", {
+  transports: ["websocket"],
+});
 var inputUsernameLogin = localStorage.getItem("vOneLocalStorage");
-console.log(inputUsernameLogin)
+console.log(inputUsernameLogin);
 if (inputUsernameLogin === null) {
-    inputUsernameLogin = "Gastnutzer";
+  inputUsernameLogin = "Gastnutzer";
 }
 
-        var form = document.getElementById("eingabeForm");
-        var input = document.getElementById("eingabe");
-        var online = document.getElementById("online");
+var form = document.getElementById("eingabeForm");
+var input = document.getElementById("eingabe");
+var online = document.getElementById("online");
 
-        appendMessage("Du bist dem Chatroom beigetreten");
-        socket.emit("new-user", inputUsernameLogin);
+appendMessage("Du bist dem Chatroom beigetreten");
+socket.emit("new-user", inputUsernameLogin);
 
-        form.addEventListener("submit", function(e) {
-            e.preventDefault();
-            if(input.value) {
-                const msg = input.value;
-                socket.emit("chat message", input.value);
-                input.value = "";
-                input.focus();
-            }
-        });
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input.value) {
+    const msg = input.value;
+    socket.emit("chat message", input.value);
+    input.value = "";
+    input.focus();
+  }
+});
 
-        socket.on("chat message", data => {
-            appendMessage(`${data.name}(${data.time}): ${data.msg}`);
-        });
+socket.on("chat message", (data) => {
+  appendMessage(`${data.name}(${data.time}): ${data.msg}`);
+});
 
-        socket.on("user-connected", name => {
-            appendMessage(`${name} ist dem Raum beigetreten`);
-        });
+socket.on("user-connected", (name) => {
+  appendMessage(`${name} ist dem Raum beigetreten`);
+});
 
-        socket.on("user-disconnected", name => {
-            appendMessage(`${name} hat den Raum verlassen`);
-        });
-        
-        socket.on("online", users => {
-            usersOnline(users);
-        });
+socket.on("user-disconnected", (name) => {
+  appendMessage(`${name} hat den Raum verlassen`);
+});
 
-        function appendMessage(msg){
-            var item = document.createElement('li');
-            item.textContent = msg;
-            ausgabe.appendChild(item);
-            window.scrollTo(0, document.body.scrollHeight);
-        }
+socket.on("online", (users) => {
+  usersOnline(users);
+});
 
-        function usersOnline(users){
-            const usernames = [];
-            for(let i = 0; i < users.length; i++){
-                let user = users[i];
-                usernames.push(user.name);
-            }
-            online.innerHTML = `<p>online: ${usernames.join(", ")}</p>`
-        }
+socket.on("admin-message", (msg) => {
+  appendMessage("Admin: " + msg);
+});
 
-        socket.on("alleMessages", data => {
-            if(data.username === "Gastnutzer") {
-                
-            }else if (data.username === inputUsernameLogin){
-                data.username = "Du";
-            }
-            appendMessage(`${data.username}(${data.time}): ${data.text}`);
-        });
+function appendMessage(msg) {
+  var item = document.createElement("li");
+  item.textContent = msg;
+  ausgabe.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+function usersOnline(users) {
+  const usernames = [];
+  for (let i = 0; i < users.length; i++) {
+    let user = users[i];
+    usernames.push(user.name);
+  }
+  online.innerHTML = `<p>online: ${usernames.join(", ")}</p>`;
+}
+
+socket.on("alleMessages", (data) => {
+  if (data.username === "Gastnutzer") {
+  } else if (data.username === inputUsernameLogin) {
+    data.username = "Du";
+  }
+  appendMessage(`${data.username}(${data.time}): ${data.text}`);
+});
--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
 const socketio = require("socket.io");
 const moment = require("moment");
 const bcrypt = require("bcrypt");
@@ -9,178 +9,218 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-var users = {}
-const usersArr = []
-var usersObj = {}
+var users = {};
+const usersArr = [];
+var usersObj = {};
 var AdminWhitelist = [];
 var usersInChatroom = [];
 
-const AdminUsername = "$2b$10$CZZV.J6.NiFJ04A83VFkCuxc8Ohc8ZXgvoMq9tXxF9rNt58sSbgSi";     //Admin
-const AdminPassword = "$2b$10$7NivWKUNsXSCWNM2B16NXe16cMcXs/UrEI5K5KrGFtl8iQaEz0sI6";     //12345678
+const AdminUsername =
+  "$2b$10$CZZV.J6.NiFJ04A83VFkCuxc8Ohc8ZXgvoMq9tXxF9rNt58sSbgSi"; //Admin
+const AdminPassword =
+  "$2b$10$7NivWKUNsXSCWNM2B16NXe16cMcXs/UrEI5K5KrGFtl8iQaEz0sI6"; //12345678
 
 const db = mysql.createConnection({
-  host        : "localhost",
-  user        : "root",
-  password    : "123456",
-  database    : "chatapp"
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "chatapp",
 });
 
 //chatapp: messages (id, username, text, time), users (id, username, password)
 
 db.connect((err) => {
-  if(err) throw err;
-  console.log("Database connected")
+  if (err) throw err;
+  console.log("Database connected");
 });
 
 //User Nutzung
-io.on('connection', (socket) =>{
-    console.log('a user connected');
-    socket.on("new-user", name => {
-        let sql = `SELECT * FROM messages`
-        let query = db.query(sql, (err, result) => {
-            result.forEach((res) => {
-                socket.emit("alleMessages", res);
-            })
-        })
-        usersInChatroom.push(socket.id);
-        users[socket.id] = name;
-        usersObj = {name: name, id: socket.id}
-        usersArr.push(usersObj);
-        socket.broadcast.emit("user-connected", name)
-        io.emit("online", usersArr);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("new-user", (name) => {
+    let sql = `SELECT * FROM messages`;
+    let query = db.query(sql, (err, result) => {
+      result.forEach((res) => {
+        socket.emit("alleMessages", res);
+      });
     });
-    socket.on("chat message", (msg) => {
-        let message = {username: users[socket.id], text: msg, time: moment().format("h:mm a")};
-        let sql = "INSERT INTO messages SET ?"
-        let query = db.query(sql, message, (err, result) => {
-          if(err) throw err;
-          socket.broadcast.emit("chat message", {msg: msg, name: users[socket.id], time: moment().format("h:mm a")});
-        socket.emit("chat message", {msg: msg, name: "Du", time: moment().format("h:mm a")});
-        });
+    usersInChatroom.push(socket.id);
+    users[socket.id] = name;
+    usersObj = { name: name, id: socket.id };
+    usersArr.push(usersObj);
+    socket.broadcast.emit("user-connected", name);
+    io.emit("online", usersArr);
+  });
+  socket.on("chat message", (msg) => {
+    let message = {
+      username: users[socket.id],
+      text: msg,
+      time: moment().format("h:mm a"),
+    };
+    let sql = "INSERT INTO messages SET ?";
+    let query = db.query(sql, message, (err, result) => {
+      if (err) throw err;
+      socket.broadcast.emit("chat message", {
+        msg: msg,
+        name: users[socket.id],
+        time: moment().format("h:mm a"),
+      });
+      socket.emit("chat message", {
+        msg: msg,
+        name: "Du",
+        time: moment().format("h:mm a"),
+      });
     });
-    socket.on('disconnect', () => {
-      console.log('user disconnected ' + socket.id);
-      if(usersInChatroom.indexOf(socket.id) >= 0) {
-        socket.broadcast.emit("user-disconnected", users[socket.id])
-        usersInChatroom.splice(usersInChatroom.indexOf(socket.id),1)[0];
-      }
-      delete users[socket.id];
-      const index = usersArr.findIndex(user => user.id === socket.id);
-      if(index !== -1) {
-        usersArr.splice(index, 1)[0];
-      }
-      io.emit("online", usersArr);
-    });
-
-    //Admin Login
-    socket.on("login-try", async data => {
-      if(await bcrypt.compare(data.username, AdminUsername) && await bcrypt.compare(data.password, AdminPassword)){
-        socket.emit("access");
-        AdminWhitelist.push(data.ip);
-        app.get("/admin", (req, res) => {
-          res.sendFile(__dirname + "/public" + "/server.html");
-        })
-        console.log(AdminWhitelist);
-      }else{
-        socket.emit("denied");
-      }
-    })
-    socket.on("admin-connected", ip => {
-      for(let i = 0; i < AdminWhitelist.length; i++){
-        if(ip == AdminWhitelist[i]){
-          socket.emit("admin-welcome-message", "Willkommen Admin");
-        }else{
-          socket.emit("admin-verweigert", "Zutritt verweigert, bitte erst anmelden");
-          socket.disconnect();
-        }
-      }
-    })
-
-    //Admin Konsole
-    socket.on("Befehl", befehl => {
-      alleBefehle = ["alle Befehle: ","!help", "!allUsers", "$kick id"];
-      if(befehl.charAt(0) == "$"){
-        let kickID = befehl.slice(6);
-        console.log(kickID)
-        io.to(kickID).emit("chat message", {msg: "Du wurdest gekickt", name: "Admin", time: moment().format("h:mm a")});
-        io.sockets.sockets.forEach((socket) => {
-          if(socket.id === kickID)
-              socket.disconnect(true);
-        });
-      }
-      if(befehl == "!help"){
-        socket.emit("help", alleBefehle);
-      }else if(befehl == "!allUsers") {
-        socket.emit("allUsers", usersArr);
-      }
-    })
-
-    // USER LOGIN
-    // Registrierung
-    socket.on("registerUser", userdata => {
-      let sql = `SELECT * FROM users WHERE username = ${mysql.escape(userdata.username)}`;
-      let query = db.query(sql, (err, result) => {
-          if(err) throw err;
-          if(Object.keys(result).length > 0){
-              socket.emit("userExists");
-          }else{
-              const pw = bcrypt.hash(userdata.password, 10);
-              pw.then(function(result){
-                  let hashedpw = result;
-                  let user = {username: userdata.username, password: hashedpw};
-                  let sql = "INSERT INTO users SET ?";
-                  let query = db.query(sql, user, (err, result) => {
-                  if(err) throw err;
-                  socket.emit("userCreated");
-                  });
-              });
-          }
-        });
-    });
-
-    //Login
-    socket.on("loginTry", userdata => {
-        let sql = `SELECT * FROM users WHERE username = ${mysql.escape(userdata.username)}`;
-        let query = db.query(sql, (err, result) => {
-            if(err) throw err;
-            if(Object.keys(result).length == 0){
-                socket.emit("user-doesnt-exist");
-            }else{
-                let sql = `SELECT password FROM users WHERE username = "${userdata.username}"`;
-                let query = db.query(sql, (err, result) => {
-                    if(err) throw err;
-                    setValue(result, userdata.password);
-                })
-            }
-        });
-    });
-    function setValue(pw, userPw){
-        let savedPw = pw[0].password;
-        pwCheck(savedPw, userPw)
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected " + socket.id);
+    if (usersInChatroom.indexOf(socket.id) >= 0) {
+      socket.broadcast.emit("user-disconnected", users[socket.id]);
+      usersInChatroom.splice(usersInChatroom.indexOf(socket.id), 1)[0];
     }
-
-    async function pwCheck(savedPw, enteredPw){
-        if(await bcrypt.compare(enteredPw, savedPw)){
-            socket.emit("access");
-        }else{
-            socket.emit("denied");
-        }
+    delete users[socket.id];
+    const index = usersArr.findIndex((user) => user.id === socket.id);
+    if (index !== -1) {
+      usersArr.splice(index, 1)[0];
     }
+    io.emit("online", usersArr);
+  });
+
+  //Admin Login
+  socket.on("login-try", async (data) => {
+    if (
+      (await bcrypt.compare(data.username, AdminUsername)) &&
+      (await bcrypt.compare(data.password, AdminPassword))
+    ) {
+      socket.emit("access");
+      AdminWhitelist.push(data.ip);
+      app.get("/admin", (req, res) => {
+        res.sendFile(__dirname + "/public" + "/server.html");
+      });
+      console.log(AdminWhitelist);
+    } else {
+      socket.emit("denied");
+    }
+  });
+  socket.on("admin-connected", (ip) => {
+    if (AdminWhitelist.includes(ip)) {
+      socket.emit("admin-welcome-message", "Willkommen Admin");
+    } else {
+      socket.emit(
+        "admin-verweigert",
+        "Zutritt verweigert, bitte erst anmelden"
+      );
+      socket.disconnect();
+    }
+  });
+
+  //Admin Konsole
+  socket.on("Befehl", (befehl) => {
+    alleBefehle = [
+      "alle Befehle: ",
+      "!help",
+      "!allUsers",
+      "$kick <id>",
+      "$message <message>",
+    ];
+    if (befehl.slice(0, 5) == "$kick") {
+      let kickID = befehl.slice(6);
+      console.log(kickID);
+      io.to(kickID).emit("chat message", {
+        msg: "Du wurdest gekickt",
+        name: "Admin",
+        time: moment().format("h:mm a"),
+      });
+      io.sockets.sockets.forEach((socket) => {
+        if (socket.id === kickID) socket.disconnect(true);
+      });
+    }
+    if (befehl.slice(0, 8) === "$message") {
+      let msg = befehl.slice(9);
+      socket.broadcast.emit("admin-message", msg);
+    }
+    if (befehl == "!help") {
+      socket.emit("help", alleBefehle);
+    } else if (befehl == "!allUsers") {
+      socket.emit("allUsers", usersArr);
+    }
+  });
+
+  // USER LOGIN
+  // Registrierung
+  socket.on("registerUser", (userdata) => {
+    let sql = `SELECT * FROM users WHERE username = ${mysql.escape(
+      userdata.username
+    )}`;
+    let query = db.query(sql, (err, result) => {
+      if (err) throw err;
+      if (Object.keys(result).length > 0) {
+        socket.emit("userExists");
+      } else {
+        const pw = bcrypt.hash(userdata.password, 10);
+        pw.then(function (result) {
+          let hashedpw = result;
+          let user = { username: userdata.username, password: hashedpw };
+          let sql = "INSERT INTO users SET ?";
+          let query = db.query(sql, user, (err, result) => {
+            if (err) throw err;
+            socket.emit("userCreated");
+          });
+        });
+      }
+    });
+  });
+
+  //Login
+  socket.on("loginTry", (userdata) => {
+    let sql = `SELECT * FROM users WHERE username = ${mysql.escape(
+      userdata.username
+    )}`;
+    let query = db.query(sql, (err, result) => {
+      if (err) throw err;
+      if (Object.keys(result).length == 0) {
+        socket.emit("user-doesnt-exist");
+      } else {
+        let sql = `SELECT password FROM users WHERE username = "${userdata.username}"`;
+        let query = db.query(sql, (err, result) => {
+          if (err) throw err;
+          setValue(result, userdata.password);
+        });
+      }
+    });
+  });
+  function setValue(pw, userPw) {
+    let savedPw = pw[0].password;
+    pwCheck(savedPw, userPw);
+  }
+
+  async function pwCheck(savedPw, enteredPw) {
+    if (await bcrypt.compare(enteredPw, savedPw)) {
+      socket.emit("access");
+    } else {
+      socket.emit("denied");
+    }
+  }
 
   //Passwort aendern
-  socket.on("change-password", data => {
-      const pw = bcrypt.hash(data.password, 10);
-      pw.then(function(result){
-          let hashedpw = result;
-          let sql = 'UPDATE users SET password = "' + hashedpw + '" WHERE username = "' + data.username +'"';
-          let query = db.query(sql, (err, result) => {
-              if(err) throw err;
-              socket.emit("changed");
-          });
+  socket.on("change-password", (data) => {
+    const pw = bcrypt.hash(data.password, 10);
+    pw.then(function (result) {
+      let hashedpw = result;
+      let sql =
+        'UPDATE users SET password = "' +
+        hashedpw +
+        '" WHERE username = "' +
+        data.username +
+        '"';
+      let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        socket.emit("changed");
       });
+    });
   });
 });
 
 server.listen(3000, "192.168.178.104", () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
